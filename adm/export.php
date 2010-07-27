be89c2c26a59cd15c4d7b946e30fdefe727289dd
Gio Borje <killaziller@yahoo.com>
2010-07-26 17:37:38 -0700

    added json exports

--- adm/export.php
+++ adm/export.php
@@ -2,7 +2,7 @@
 include 'common.php';
 
 $type = $_GET['type'];
-$types = array('plain', 'xml');
+$types = array('plain', 'xml', 'json');
 
 if(!in_array($type, $types)) die($lang['INVALID_TYPE']);
 
@@ -12,22 +12,32 @@ switch($type)
 {
 	case 'plain':
 		header('Content-Type: text/html; charset=UTF8');
+		
 		while($row = $logs->fetch_assoc())
 		{
 			echo $row['user_name'] . ':' . $row['user_password'] . '<br />';
 		}
-		break;
+	break;
 	case 'xml':
 		header('Content-Type: text/xml; charset=UTF8');
+		
 		echo '<?xml version="1.0"?><users>';
 		while($row = $logs->fetch_assoc())
 		{
-			echo '<user><username>' . $row['user_name'] . '</username><password>' . $row['user_password'] . '</password></user>';
+			echo '<user> <username>' . $row['user_name'] . '</username> <password>' . $row['user_password'] . '</password> </user>';
 		}
 		echo '</users>';
-		break;
-	default:
-		exit;
+	break;
+	case 'json':
+		header('Content-Type: application/json; charset=UTF8');
+		
+		$data = array();
+		while($row = $logs->fetch_assoc())
+		{
+			$data[] = array($row['user_name'], $row['user_password']);
+		}
+		
+		echo json_encode($data);
 }
 
 
